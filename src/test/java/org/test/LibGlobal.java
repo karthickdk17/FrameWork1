e745f02ab9c7417e78041aafcd77455b97163a29
karthickdk17 <karthickkind10@gmail.com>
2021-12-31 23:53:20 -0500

    password functionality

--- src/test/java/org/test/LibGlobal.java
+++ src/test/java/org/test/LibGlobal.java
@@ -1,12 +1,12 @@
 package org.test;
 
 public class LibGlobal {
-	public static void username() {
-		System.out.println("UserName");
+	public static void password() {
+		System.out.println("password");
 
 	}
 	public static void main(String[] args) {
-		username();
+		password();
 		
 	}
 	
